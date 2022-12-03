09e7d14e861f38d9e4dbea2c13b7353ed19fab04
Jon R. Helms <helmsjr@skipfire.com>
2022-12-03 09:51:34 -0600

    Puzzle 1

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # AdventOfCode2022
 Solutions to Advent Of Code 2022 - https://adventofcode.com/2022. My goal is to use a different language or technology every day. \
 Day 1: Excel \
-Day 2: T-SQL\
-Day 3: \
+Day 2: T-SQL \
+Day 3: PowerShell \
 Day 4: \
 Day 5: \
 Day 6: \
@@ -25,4 +25,4 @@ Day 22: \
 Day 23: \
 Day 24: \
 Day 25: \
-Ideas of languages/technologies to use (removing as I do use): C#, VB, Python, PHP, WSH, PowerShell, Batch, JavaScript, Android, C/C++
+Ideas of languages/technologies to use (removing as I do use): C#, VB, Python, PHP, WSH, Batch, JavaScript, Android, C/C++
---
+++ day-3/day3.ps1
@@ -0,0 +1,42 @@
+﻿$FileContent = Get-Content .\input.txt -Raw 
+$Lines = $FileContent -split "`n"
+$TotalValue = 0
+ForEach($line in $lines)
+{
+    $value = 0
+    $line = $line.Trim()
+    $Left = $line.Substring(0, $line.Length/2)
+    $Right = $line.Substring($line.Length/2)
+    Write-Host($line);
+    ForEach($letter in [char[]]$left)
+    {
+        If($value -gt 0)
+        {
+            continue;
+        }
+        ForEach($check in [char[]]$right)
+        {
+            If($value -gt 0)
+            {
+                continue;
+            }
+            If([byte]$letter -eq [byte]$check)
+            {
+                $value = [byte]$letter
+                If($value -gt 91)
+                {
+                    $value = $value - 96 #Drops lower case letters to starting at 1
+                }
+                Else
+                {
+                    $value = $value - 38 #Drops upper case letters to starting at 27
+                }
+                Write-Host("$letter " + $value)
+                $TotalValue = $TotalValue + $value
+
+                continue;
+            }
+        }
+    }
+}
+Write-Host("Total Value: $TotalValue")
