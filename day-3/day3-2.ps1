b347ab41981021f4a0d4cae6a9f2e3fb7f3fc037
Jon R. Helms <helmsjr@skipfire.com>
2022-12-03 10:02:43 -0600

    Puzzle 2

---
+++ day-3/day3-2.ps1
@@ -0,0 +1,73 @@
+﻿$FileContent = Get-Content .\input.txt -Raw 
+$Lines = $FileContent -split "`n"
+$TotalValue = 0
+$Line1 = ""
+$Line2 = ""
+$Line3 = ""
+ForEach($line in $lines)
+{
+    $line = $line.Trim()
+    If($Line1.Length -eq 0)
+    {
+        $Line1 = $line
+    }
+    ElseIf($Line2.Length -eq 0)
+    {
+        $Line2 = $line
+    }
+    ElseIf($Line3.Length -eq 0)
+    {
+        $Line3 = $line
+        Write-Host($Line1)
+        Write-Host($Line2)
+        Write-Host($Line3)
+
+        $value = 0
+        ForEach($letter1 in [char[]]$line1)
+        {
+            If($value -gt 0)
+            {
+                continue;
+            }
+            Write-Host("Checking $letter1")
+            ForEach($letter2 in [char[]]$line2)
+            {
+                If($value -gt 0)
+                {
+                    continue;
+                }
+                If([byte]$letter1 -eq [byte]$letter2)
+                {
+                    ForEach($letter3 in [char[]]$line3)
+                    {
+                        If($value -gt 0)
+                        {
+                            continue;
+                        }
+                        If([byte]$letter1 -eq [byte]$letter3)
+                        {
+                            $value = [byte]$letter1
+                            If($value -gt 91)
+                            {
+                                $value = $value - 96 #Drops lower case letters to starting at 1
+                            }
+                            Else
+                            {
+                                $value = $value - 38 #Drops upper case letters to starting at 27
+                            }
+                            Write-Host("$letter1 " + $value)
+                            $TotalValue = $TotalValue + $value
+
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+
+        $Line1 = ""
+        $Line2 = ""
+        $Line3 = ""
+    }
+}
+Write-Host("Total Value: $TotalValue")
