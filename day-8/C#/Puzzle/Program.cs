3f6843156adba37d5541587c12e920b30b479453
Jon R. Helms <jon.helms@dutchie.com>
2022-12-08 06:36:01 -0600

    Puzzle 2

--- day-8/C#/Puzzle/Program.cs
+++ day-8/C#/Puzzle/Program.cs
@@ -12,19 +12,33 @@ var maxY = trees.Max(t => t.y);
 foreach (var tree in trees)
 {
     if (tree.x == 0 || tree.y == 0 || tree.x == maxX || tree.y == maxY)
+    {
         tree.IsVisible = true; //The tree is on the edge so it is visible.
+        tree.ScenicScore = 0; //Trees on the edge have a 0 for one direction, making the scneic result always 0.
+    }
     else
     {
-        var treeBlockers = trees.Where(t => ( t.y.Equals(tree.y) || t.x.Equals(tree.x) ) && t.h >= tree.h).ToList(); //ToList gets rid of multiple enumeration warning
-        var blockedLeft = treeBlockers.Any(t => t.y.Equals(tree.y) && t.x < tree.x);
-        var blockedRight = treeBlockers.Any(t => t.y.Equals(tree.y) && t.x > tree.x);
-        var blockedUp = treeBlockers.Any(t => t.x.Equals(tree.x) && t.y < tree.y);
-        var blockedDown = treeBlockers.Any(t => t.x.Equals(tree.x) && t.y > tree.y);
-        tree.IsVisible = !( blockedDown && blockedUp && blockedRight && blockedLeft );
+        //ToList gets rid of multiple enumeration warning
+        //Get all blockers of this tree
+        var treeBlockers = trees.Where(t => ( t.y.Equals(tree.y) || t.x.Equals(tree.x) ) && t.h >= tree.h).ToList();
+        //Get the blockers each direction
+        var blockersLeft = treeBlockers.Where(t => t.y.Equals(tree.y) && t.x < tree.x).ToList();
+        var blockersRight = treeBlockers.Where(t => t.y.Equals(tree.y) && t.x > tree.x).ToList();
+        var blockersUp = treeBlockers.Where(t => t.x.Equals(tree.x) && t.y < tree.y).ToList();
+        var blockersDown = treeBlockers.Where(t => t.x.Equals(tree.x) && t.y > tree.y).ToList();
+        //If the tree has blockers in all directions it is not visible.
+        tree.IsVisible = !( blockersDown.Any() && blockersUp.Any() && blockersRight.Any() && blockersLeft.Any() );
+
+        var rangeLeft = tree.x - (blockersLeft.Any() ? blockersLeft.Max(t => t.x) : 0);
+        var rangeRight = (blockersRight.Any() ? blockersRight.Min(t => t.x) : maxX) - tree.x;
+        var rangeUp = tree.y - (blockersUp.Any() ? blockersUp.Max(t => t.y) : 0);
+        var rangeDown = (blockersDown.Any() ? blockersDown.Min(t => t.y) : maxY) - tree.y;
+        tree.ScenicScore = rangeLeft * rangeRight * rangeUp * rangeDown;
     }
 }
 
 Console.WriteLine("Puzzle 1: " + trees.Count(t => t.IsVisible));
+Console.WriteLine("Puzzle 2: " + trees.Max(t => t.ScenicScore));
 
 class Tree
 {
@@ -32,6 +46,7 @@ class Tree
     public int y { get; set; }
     public int h { get; set; }
     public bool IsVisible { get; set; } = false;
+    public int ScenicScore { get; set; }
 
     public Tree(int x, int y, char h)
     {
